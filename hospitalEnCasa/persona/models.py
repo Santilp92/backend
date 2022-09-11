64bd8af6ef38c64258efd70c14376f56df6186d0
Santiago Lopez <salp7811@hotmail.com>
2022-09-11 15:04:20 -0500

    creacion de modelo base de datos con sqlite

--- hospitalEnCasa/persona/models.py
+++ hospitalEnCasa/persona/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import datetime
+
 
 # Create your models here.
+class Persona(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    celular = models.BigIntegerField(default=0)
+
+class Paciente(Persona):
+    direccion = models.CharField(max_length=50)
+    ciudad = models.CharField(max_length=50)
+    fechaNacimiento= models.DateField(("Date"), default= datetime.date.today)
+
+class Familiar(Paciente):
+    parentesco= models.CharField(max_length=45)
+    correo = models.CharField(max_length=45)
+
+class Doctor(Persona):
+    registro = models.AutoField(primary_key=True)
+    especialidad = models.CharField(max_length=45)
+
--- hospitalEnCasa/persona/views.py
+++ hospitalEnCasa/persona/views.py
@@ -5,3 +5,37 @@ from django.http import HttpResponse
 
 def consultarPaciente(request):
     return HttpResponse("Consultar paciente")
+
+def newMember(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            #print(data)
+            #print(type(data))
+            member = Members(
+                name = data["name"],
+                email = data["email"]
+            )
+            #print(member)
+            member.save()
+            return HttpResponse("Va a agragar un nuevo miembro")
+        except:
+            return HttpResponseBadRequest("Error en los datos enviados")
+    else:
+        return HttpResponseNotAllowed(['POST'], "Método inválido")
+
+def getMember(request):
+    if request.method == 'GET':
+        members = Members.objects.all()
+        allMemberData = []
+        for x in members:
+            data = {"id": x.id, "name": x.name, "email":x.email}
+            allMemberData.append(data)
+        dataJson = json.dumps(allMemberData)
+        #print(dataJson)
+        resp = HttpResponse()
+        resp.headers['Content-Type'] = "text/json"
+        resp.content = dataJson
+        return resp
+    else:
+        return HttpResponseNotAllowed(['GET'], "Método inválido")
