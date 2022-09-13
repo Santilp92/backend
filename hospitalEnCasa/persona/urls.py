ccc342995b672c217ecb35e64d3539a7a39fa7f1
Santiago Lopez <salp7811@hotmail.com>
2022-09-12 22:27:08 -0500

    Se finalizan las funciones y los modelos

--- hospitalEnCasa/persona/urls.py
+++ hospitalEnCasa/persona/urls.py
@@ -2,10 +2,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('nuevaPersona', views.nuevaPersona, name = 'registroPersona'),
-    path('consultarPacientes', views.consultarPacientes, name = 'ConsultarTodos'), 
-    path('buscarPaciente/<int:id>', views.buscarPaciente, name = 'ConsultarUno'), 
+    #urls de auxiliar
+    path('auxiliar/nuevoPaciente', views.nuevaPersonaPaciente, name = 'RegistroPaciente'),
+    path('auxiliar/consultarPacientes', views.consultarPersonasPacientes, name = 'ConsultarPacientes'),
+    path('auxiliar/actualizarPaciente/<int:id>', views.actualizarPersonaPaciente, name = 'ActualizarPaciente'),
+    path('auxiliar/borrarPaciente/<int:id>', views.borrarPersonaPaciente, name = 'EliminarPaciente'),
+    path('auxiliar/buscarPersonaPaciente/<int:id>', views.buscarPersonaPaciente, name = 'BuscarPaciente'),
 
+
+    #urls Persona
+    path('nuevaPersona', views.nuevaPersona, name = 'RegistroPersona'),
+    path('consultarPersonas', views.consultarPersonas, name = 'ConsultarTodos'), 
+    path('buscarPersona/<int:id>', views.buscarPersona, name = 'ConsultarUno'), 
+    path('actualizarPersona/<int:id>', views.actualizarPersona, name = 'Actualizar'),
+    path('borrarPersona/<int:id>', views.borrarPersona, name = 'Eliminar'),
+
+    #urls paciente
+    path('actualizarPaciente/<int:id>', views.actualizarPaciente, name = 'actualizarPaciente'),
+    path('nuevoPaciente', views.nuevoPaciente, name = 'RegistroPaciente'),
+    path('borrarPaciente/<int:id>', views.borrarPaciente, name = 'EliminarPaciente'),
     # path('nuevoPaciente', views.nuevoPaciente, name = 'registroPaciente'),
     # path('nuevoDoctor', views.nuevoDoctor, name = 'registroDoctor'),
     
--- hospitalEnCasa/persona/views.py
+++ hospitalEnCasa/persona/views.py
@@ -1,13 +1,132 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed
 import json
-
-from .models import Persona
+from .models import Persona,Paciente
 
 
 # Create your views here.
-def nuevaPersona(request):
+#----------------------
+#funciones del auxiliar
+#----------------------
+def nuevaPersonaPaciente(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            persona = Persona(
+                id = data ["id"],
+                nombres = data["nombres"],
+                apellidos = data["apellidos"],
+                celular = data["celular"]
+                )
+            persona.save()
+            persona = Persona.objects.filter(id= data["id"]).first()
+            if (not persona):
+                return HttpResponseBadRequest("No existe la persona con esa cédula")
+            paciente = Paciente(
+                idPaciente = persona,
+                direccion = data["direccion"],
+                ciudad = data["ciudad"],
+                fechaNacimiento = data["fechaNacimiento"]
+                )
+            paciente.save()
+            return HttpResponse("Nuevo paciente agregado")
+        except:
+            return HttpResponseBadRequest("Error en los datos enviados")
+    else:
+        return HttpResponseNotAllowed(['POST'], "Método inválido")
+
+def actualizarPersonaPaciente(request, id):
+    if request.method == 'PUT':
+        try:
+            persona = Persona.objects.filter(id=id).first()
+            if (not persona):
+                return HttpResponseBadRequest("No existe la persona con esa cédula")
+            data = json.loads(request.body)
+            persona.celular = data["celular"]
+            persona.save()
+
+            paciente = Paciente.objects.filter(idPaciente = id).first()
+            if (not paciente):
+                return HttpResponseBadRequest("No existe el paciente")
+            data = json.loads(request.body)
+            paciente.direccion = data["direccion"]
+            paciente.ciudad = data["ciudad"]
+            paciente.save()
+            return HttpResponse("Paciente actualizado")
+        except:
+            return HttpResponseBadRequest("Error en los datos enviados")
+    else:
+        return HttpResponseNotAllowed(['PUT'], "Método inválido")
+
+def consultarPersonasPacientes(request):
     if request.method == 'GET':
+        personas = Persona.objects.all()
+        if (not personas):
+            return HttpResponseBadRequest("No existen personas en la bd")
+        personasData = []
+        for x in personas:
+            data = {"nombres": x.nombres, "apellidos":x.apellidos}
+            personasData.append(data)
+        dataJson = json.dumps(personasData)
+        resp = HttpResponse()
+        resp.headers['Content-Type'] = "text/json"
+        resp.content = dataJson
+        return resp
+    else:
+        return HttpResponseNotAllowed(['GET'], "Método inválido")
+
+def borrarPersonaPaciente(request, id):
+    if request.method == 'DELETE':
+        try:
+            paciente = Paciente.objects.filter(idPaciente=id).first()
+            if (not paciente):
+                return HttpResponseBadRequest("No existe pacientes con esta cédula")
+            paciente.delete()
+
+            persona = Persona.objects.filter(id=id).first()
+            if (not persona):
+                return HttpResponseBadRequest("No existe persona con esta cédula")
+            persona.delete()
+            return HttpResponse("Paciente eliminado")
+        except:
+            return HttpResponseBadRequest("Error en los datos enviados")
+    else:
+        return HttpResponseNotAllowed(['DELETE'], "Método inválido")
+
+def buscarPersonaPaciente(request, id):
+    if request.method == 'GET':
+        persona = Persona.objects.filter(id=id).first()
+        if (not persona):
+            return HttpResponseBadRequest("No existe paciente con esta cédula")
+        
+        paciente = Paciente.objects.filter(idPaciente = id).first()
+        pacienteData = []
+        for dato in paciente:
+            data = {"direccion":dato.direccion,"ciudad":dato.ciudad,
+            "fechaNacimiento":dato.fechaNacimiento}
+            pacienteData.append(data)
+        data = {
+            "id":persona.id, 
+            "nombres": persona.nombres, 
+            "apellidos":persona.apellidos, 
+            "celular":persona.celular,
+            "paciente":pacienteData
+            }
+
+        dataJson = json.dumps(data)
+        resp = HttpResponse()
+        resp.headers['Content-Type'] = "text/json"
+        resp.content = dataJson
+        return resp
+    else:
+        return HttpResponseNotAllowed(['GET'], "Método inválido")
+
+#----------------------
+#Persona
+#----------------------
+
+def nuevaPersona(request):
+    if request.method == 'POST':
         try:
             data = json.loads(request.body)
             persona = Persona(
@@ -23,14 +142,13 @@ def nuevaPersona(request):
     else:
         return HttpResponseNotAllowed(['POST'], "Método inválido")
 
-def consultarPacientes(request):
+def consultarPersonas(request):
     if request.method == 'GET':
         personas = Persona.objects.all()
         if (not personas):
             return HttpResponseBadRequest("No existen personas en la bd")
         personasData = []
         for x in personas:
-            #fechaStr =x.fechaNacimiento.strftime("%Y-%m-%d",)
             data = {"nombres": x.nombres, "apellidos":x.apellidos}
             personasData.append(data)
         dataJson = json.dumps(personasData)
@@ -41,7 +159,7 @@ def consultarPacientes(request):
     else:
         return HttpResponseNotAllowed(['GET'], "Método inválido")
 
-def buscarPaciente(request, id):
+def buscarPersona(request, id):
     if request.method == 'GET':
         persona = Persona.objects.filter(id=id).first()
         if (not persona):
@@ -55,6 +173,85 @@ def buscarPaciente(request, id):
         return resp
     else:
         return HttpResponseNotAllowed(['GET'], "Método inválido")
+
+def actualizarPersona(request, id):
+    if request.method == 'PUT':
+        try:
+            persona = Persona.objects.filter(id=id).first()
+            if (not persona):
+                return HttpResponseBadRequest("No existe la persona con esa cédula")
+            data = json.loads(request.body)
+            persona.nombres = data["nombres"]
+            persona.apellidos = data["apellidos"]
+            persona.celular = data["celular"]
+            persona.save()
+            return HttpResponse("Persona actualizada")
+        except:
+            return HttpResponseBadRequest("Error en los datos enviados")
+    else:
+        return HttpResponseNotAllowed(['PUT'], "Método inválido")
+
+def borrarPersona(request, id):
+    if request.method == 'DELETE':
+        try:
+            persona = Persona.objects.filter(id=id).first()
+            if (not persona):
+                return HttpResponseBadRequest("No existe la persona con esa cédula")
+            persona.delete()
+            return HttpResponse("Persona eliminada")
+        except:
+            return HttpResponseBadRequest("Error en los datos enviados")
+    else:
+        return HttpResponseNotAllowed(['DELETE'], "Método inválido")
+
+
+#----------------------
+#Paciente
+#----------------------
+
+def nuevoPaciente(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            persona = Persona.objects.filter(id= data["id"]).first()
+            if (not persona):
+                return HttpResponseBadRequest("No existe la persona con esa cédula")
+            paciente = Paciente(
+                idPaciente = persona,
+                direccion = data["direccion"],
+                ciudad = data["ciudad"],
+                fechaNacimiento = data["fechaNacimiento"]
+                )
+            paciente.save()
+            return HttpResponse("Nuevo paciente agregado")
+        except:
+            return HttpResponseBadRequest("Error en los datos enviados")
+    else:
+        return HttpResponseNotAllowed(['POST'], "Método inválido")
+
+def actualizarPaciente(request, id):
+    if request.method == 'PUT':
+        try:
+            paciente = Paciente.objects.filter(idPaciente = id).first()
+            if (not paciente):
+                return HttpResponseBadRequest("No existe el paciente")
+            data = json.loads(request.body)
+            paciente.direccion = data["direccion"]
+            paciente.ciudad = data["ciudad"]
+            paciente.save()
+            return HttpResponse("Paciente actualizado")
+        except:
+            return HttpResponseBadRequest("Error en los datos enviados")
+    else:
+        return HttpResponseNotAllowed(['PUT'], "Método inválido") 
+
+
+
+
+
+
+
+
 
 # def nuevoPaciente(request):
 #     if request.method == 'POST':
@@ -129,3 +326,15 @@ def buscarPaciente(request, id):
     else:
         return HttpResponseNotAllowed(['POST'], "Método inválido")
 
+def borrarPaciente(request, id):
+    if request.method == 'DELETE':
+        try:
+            paciente = Paciente.objects.filter(idPaciente=id).first()
+            if (not paciente):
+                return HttpResponseBadRequest("No existe la persona con esa cédula")
+            paciente.delete()
+            return HttpResponse("Paciente eliminado")
+        except:
+            return HttpResponseBadRequest("Error en los datos enviados")
+    else:
+        return HttpResponseNotAllowed(['DELETE'], "Método inválido")
